fa04856a5fa47df7de05998cdcfb362d625d6e1c
Ben Foster <benjaminafoster4@gmail.com>
2026-01-16 15:21:49 -0500

    added logic to build config based on environment variables

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from os import environ
 from Configuration import Config
@@ -11,10 +12,24 @@ CURRENT_DIRECTORY = Path(__file__).parent.resolve()
 CONFIG_DIRECTORY = CURRENT_DIRECTORY / 'config'
 TARGETS_FILE_PATH = CONFIG_DIRECTORY / 'Targets.yaml'
 
+# Load environment variables
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except Exception:
+    pass
+nemo_env = environ.get('NEMO_ENV', 'testing')
 
-        
-config = Config(config_path=TARGETS_FILE_PATH, timeout=3)
+# Create config object based on environment
+if nemo_env == "testing":
+    config = Config(config_path=CONFIG_DIRECTORY / 'test-targets.yaml')
+elif nemo_env == "development":
+    print("No development hosts defined...yikes")
+    sys.exit(1)
+else:
+    config = Config(config_path=TARGETS_FILE_PATH)
 
+# test logic below
 scanner = Scanner(config)
 
 scanner.host_icmp_scan()
